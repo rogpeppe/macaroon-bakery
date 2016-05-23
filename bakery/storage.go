f6adf0281755bfd6b6a9407db7e17bf7d1341957
Roger Peppe <rogpeppe@gmail.com>
2016-05-23 18:03:38 +0100

    bakery: add RootKeyStorage

--- bakery/storage.go
+++ bakery/storage.go
@@ -9,6 +9,10 @@ import (
 
 // Storage defines storage for macaroons.
 // Calling its methods concurrently is allowed.
+//
+// This type is the original storage interface supported
+// by the bakery and will eventually be deprecated
+// in favour of RootKeyStorage.
 type Storage interface {
 	// Put stores the item at the given location, overwriting
 	// any item that might already be there.
@@ -24,7 +28,70 @@ type Storage interface {
 	Del(location string) error
 }
 
+// RootKeyStorage defines defines storage for macaroon
+// root keys.
+type RootKeyStorage interface {
+	// Get returns the root key for the given id.
+	// If the item is not there, it returns ErrNotFound.
+	Get(id string) ([]byte, error)
+
+	// RootKey returns the root key to be used for making a new
+	// macaroon, and an id that can be used to look it up later with
+	// the Get method.
+	//
+	// Note that the root keys should remain available for as long
+	// as the macaroons using them are valid.
+	//
+	// Note that there is no need for it to return a new root key
+	// for every call - keys may be reused, although some key
+	// cycling is over time is advisable.
+	RootKey() (rootKey []byte, id string, err error)
+}
+
+// NewFallbackStorage returns a RootKeyStorage implementation
+// that uses s0 as primary storage, but will fall back to getting
+// root keys from s1 if an id in s0 cannot be found.
+//
+// RootKey will always return root keys from s0.
+//func NewFallbackStorage(s0, s1 RootKeyStorage) RootKeyStorage {
+//}
+
 var ErrNotFound = errors.New("item not found")
+
+// NewMemRootKeyStorage returns an implementation of
+// RootKeyStorage that generates a single key and always
+// returns that from RootKey. The same id ("0") is always
+// used.
+func NewMemRootKeyStorage() RootKeyStorage {
+	return new(memRootKeyStorage)
+}
+
+type memRootKeyStorage struct {
+	mu  sync.Mutex
+	key []byte
+}
+
+func (s *memRootKeyStorage) Get(id string) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if id != "0" || s.key == nil {
+		return nil, ErrNotFound
+	}
+	return s.key, nil
+}
+
+func (s *memRootKeyStorage) RootKey() (rootKey []byte, id string, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.key == nil {
+		newKey, err := randomBytes(24)
+		if err != nil {
+			return nil, "", err
+		}
+		s.key = newKey
+	}
+	return s.key, "0", nil
+}
 
 // NewMemStorage returns an implementation of Storage
 // that stores all items in memory.
@@ -70,13 +137,13 @@ type storageItem struct {
 }
 
 // storage is a thin wrapper around Storage that
-// converts to and from StorageItems in its
+// converts to and from []byte root keys in its
 // Put and Get methods.
 type storage struct {
 	store Storage
 }
 
-func (s storage) Get(location string) (*storageItem, error) {
+func (s storage) Get(location string) ([]byte, error) {
 	itemStr, err := s.store.Get(location)
 	if err != nil {
 		return nil, err
@@ -85,10 +152,13 @@ func (s storage) Get(location string) (*storageItem, error) {
 	if err := json.Unmarshal([]byte(itemStr), &item); err != nil {
 		return nil, fmt.Errorf("badly formatted item in store: %v", err)
 	}
-	return &item, nil
+	return item.RootKey, nil
 }
 
-func (s storage) Put(location string, item *storageItem) error {
+func (s storage) Put(location string, key []byte) error {
+	item := &storageItem{
+		RootKey: key,
+	}
 	data, err := json.Marshal(item)
 	if err != nil {
 		panic(fmt.Errorf("cannot marshal storage item: %v", err))
